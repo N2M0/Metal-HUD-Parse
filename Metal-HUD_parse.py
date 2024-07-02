d7c0892d3ea96c79ef960d244682556496735cbc
N2M0 <nemonangoom@naver.com>
2024-07-02 10:44:23 +0900

    소수점 자리가 누락된 데이터 따로 저장
    일부 Frametime, GPUTime 데이터의 소수점 영역이 <...>로 들어올 경우, 해당 데이터가 몇 개나 발생했는지를 보여주고, 이렇게 들어온 데이터는 따로 빼서 저장합니다.

--- Metal-HUD_parse.py
+++ Metal-HUD_parse.py
@@ -11,11 +11,16 @@ benchmarkBasedTime = 1000
 
 hudRawData = csv.reader(HUDFile)
 missedFrame = 0  # 누락된 프레임 갯수
+frametimeError = 0 # 소수점 자리가 <...>로 입력된 프레임타임
+gpuTimeError = 0 # 소수점 자리가 <...>로 입력된 GPU타임
 
 FPSData = []  # 프레임타임을 FPS로 바꾼 것
 frameTimeData = []  # 프레임타임 데이터 (ms)
 gpuTimeData = []  # GPU타임 데이터 (ms)
 memoryData = []  # 메모리 사용량 저장
+
+frametimeErrorData = []
+gpuTimeErrorData = []
 
 secondSum = 0
 frameCount = 0
@@ -24,7 +29,7 @@ overlapCheckData = "temp"
 
 # 데이터 분리
 # 콘솔로 들어온 데이터 중 중복된 값이 있어서 제거 처리
-# 마지막 데이터가 <...>로 들어올 때가 있어서 제거 처리
+# 일부 데이터의 소수점 자리가 <...>로 들어올 때가 있어서 제거 처리
 for line in hudRawData:
     if overlapCheckData != line[0]:
             overlapCheckData = line[0]
@@ -32,11 +37,19 @@ for line in hudRawData:
             memoryData.append(line[2])
 
             for i in range(3, len(line)):
-                if ("<" not in line[i]) & (line[i] != ""):
+                if ("<" not in line[i]):
+                    if (line[i] != ""):
+                        if i % 2 == 1:
+                            frameTimeData.append(float(line[i]))
+                        else:
+                            gpuTimeData.append(float(line[i]))
+                else:
                     if i % 2 == 1:
-                        frameTimeData.append(float(line[i]))
+                        frametimeError += 1
+                        frametimeErrorData.append(line[i])
                     else:
-                        gpuTimeData.append(float(line[i]))
+                        gpuTimeError += 1
+                        gpuTimeErrorData.append(line[i])
     else:
         continue
 
@@ -71,4 +84,12 @@ gpuTimeDataFrame.to_csv('GPUTime-Result.csv')
 memoryDataFrame = pd.DataFrame({'Memory(MB)' : memoryData})
 memoryDataFrame.to_csv('Memory-Result.csv')
 
+frametimeErrorDataFrame = pd.DataFrame({'Frametime error list' : frametimeErrorData})
+frametimeErrorDataFrame.to_csv('Frametime-Error.csv')
+
+gpuTimeErrorDataFrame = pd.DataFrame({'GPUTime error list' : gpuTimeErrorData})
+gpuTimeErrorDataFrame.to_csv('GPUTime-error.csv')
+
 print("\n\nDone! Missed Frame:", missedFrame)
+print("Frametime error:", + frametimeError)
+print("GPUTime error:", + gpuTimeError)
