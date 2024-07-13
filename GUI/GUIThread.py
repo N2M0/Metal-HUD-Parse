c9281583150efab1457915df6f43a75cc704cf9a
HomeGravity <mycoding467@gmail.com>
2024-07-13 17:47:57 +0900

    스레드 관련 코드를 최적화하였고 데이터 미리보기 테이블의 코드를 세분화 했습니다.

--- GUI/GUIThread.py
+++ GUI/GUIThread.py
@@ -2,6 +2,7 @@ from PyQt5.QtCore import QThread, pyqtSignal
 from PyQt5.QtWidgets import QMessageBox
 from Metal_HUD_parse import *
 from GUIStyle import *
+from collections import ChainMap, OrderedDict
 
 # 초기화
 _PerformanceCalculationConditions = None
@@ -12,7 +13,8 @@ _PerformanceErrorData  = None
 class PerformanceParsingThread(QThread):
     # 서브 스레드에서 메인 스레드로 데이터를 보낼 신호
     UpdateFileLabelSignal = pyqtSignal(str)
-    EmitParsedSignal = pyqtSignal(list, bool, int, int, int, int, str)  # 추가된 신호
+    EmitInitializeTableSignal =  pyqtSignal(list, int, int)
+    EmitParsedSignal = pyqtSignal(int, int, str)  # 추가된 신호
     EmitParsedPbarSignal = pyqtSignal(int, int)
     ThreadFinishedSignal = pyqtSignal()
 
@@ -47,42 +49,65 @@ class PerformanceParsingThread(QThread):
         self.UpdateFileLabelSignal.emit(f'Selected File: {self.FileName} Loding(Saveable.)...')
 
         # 함수 호출
+        self.emitInitializeTableSignal()
         self.emitParsedSignal()
         self.UpdateFileLabelSignal.emit(f'Selected File: {self.FileName} Done!')
         
-        
         # 스레드 종료
         self.ThreadFinishedSignal.emit()
+        self.deleteLater()
+    
+    # 여러개의 딕셔너리를 하나의 딕셔너리로 만듦
+    def CombinedDict(self):
+        combined_dict = OrderedDict()
+        combined_dict.update(_PerformanceData)
+        combined_dict.update(_PerformanceErrorData)
+        combined_dict.update(_PerformanceCalculationConditions)
+        return combined_dict
+    
+    # 컴빈드 데이터 계산
+    def CombinedDictData(self, combined_dict):
+        return [len(v) for v in combined_dict.values() if isinstance(v, list)]
+    
+    # 테이블에 row, col 초기 설정 데이터를 전달하는 함수
+    def emitInitializeTableSignal(self):
+        combined_dict = self.CombinedDict()
+        data = self.CombinedDictData(combined_dict)
+        label_list = list(combined_dict.keys())
+        col_count = len(combined_dict)
+        row_count = max(data) # Key 값 중에 가장 많은 Value 하나 찾음.
+        self.EmitInitializeTableSignal.emit(label_list, col_count, row_count)
+        
     
     # 파싱 데이터를 테이블에 데이터를 전달하는 함수
     def emitParsedSignal(self):
-        combined_dict = {**_PerformanceData, **_PerformanceErrorData, **_PerformanceCalculationConditions}
-
-        data = [len(v) for v in combined_dict.values() if isinstance(v, list)]
-        row_count = max(data) # Key 값 중에 가장 많은 Value 하나 찾음.
+        combined_dict = self.CombinedDict()
+        data = self.CombinedDictData(combined_dict)
         sum_count = sum(data) + len(_PerformanceCalculationConditions.values()) # 프로그래스바 100% 기준 해당하는 값
-        col_count = len(combined_dict)
-        LabelList = list(combined_dict.keys())
 
         sum_pbar = 1
         for col, value in enumerate(combined_dict.values()):
             # 타입 검사
             if isinstance(value, (list, tuple)):
                 for row, item in enumerate(value):
-                    self.EmitParsedSignal.emit(LabelList, sum_pbar == 1, col_count, row_count, col, row, str(item))
-                    self.emitParsedPbarSignal(sum_pbar, sum_count)
+                    self.emitParsedSignalItem(col, row, str(item))
+                    self.emitParsedPbarValue(sum_pbar, sum_count)
                     self.overhead(sum_pbar)
                     sum_pbar += 1
 
             # 데이터 배열이 아닐때
             else:
-                self.EmitParsedSignal.emit(LabelList, sum_pbar == 1, col_count, row_count, col, 0, str(value))
-                self.emitParsedPbarSignal(sum_pbar, sum_count)
+                self.emitParsedSignalItem(col, 0, str(value))
+                self.emitParsedPbarValue(sum_pbar, sum_count)
                 self.overhead(sum_pbar)
                 sum_pbar += 1
     
+    
+    def emitParsedSignalItem(self, col, row, item):
+        self.EmitParsedSignal.emit(col, row, item)
+    
     # 진행상황 프로그래스바 신호 업데이트
-    def emitParsedPbarSignal(self, sum_pbar, sum_count):
+    def emitParsedPbarValue(self, sum_pbar, sum_count):
         self.EmitParsedPbarSignal.emit(sum_pbar, sum_count)
 
     # 메인 스레드가 업데이트를 하기위해 서브 스레드를 잠시 멈추는 함수
@@ -98,7 +123,7 @@ class PerformanceParsingResultsSaveThread(QThread):
     
     # 메시지박스
     def ShowMessagebox(self, setText):
-        msgBox = QMessageBox(self.parent)
+        msgBox = QMessageBox()
         msgBox.setWindowTitle('Parsed Save')
         msgBox.setText(setText)
         
@@ -107,14 +132,19 @@ class PerformanceParsingResultsSaveThread(QThread):
         msgBox.exec_()
     
     def run(self):
-        if all([_PerformanceCalculationConditions, _PerformanceData, _PerformanceErrorData]):
-            PerformanceCsvSave("FPS-Result.csv", f"FPS - 약 {_PerformanceCalculationConditions[benchmarkBasedTime]} ms마다 평균치 계산", _PerformanceData[FPSData])
-            PerformanceCsvSave("Frametime-Result.csv", f"Frametime", _PerformanceData[frameTimeData])
-            PerformanceCsvSave("GPUTime-Result.csv", f"GPUTime", _PerformanceData[gpuTimeData])
-            PerformanceCsvSave("Memory-Result.csv", f"Memory(MB)", _PerformanceData[memoryData])
-            PerformanceCsvSave("Frametime-Error.csv", f"Frametime error list", _PerformanceErrorData[frametimeErrorData])
-            PerformanceCsvSave("GPUTime-error.csv", f"GPUTime error list", _PerformanceErrorData[gpuTimeErrorData])
-            self.ShowMessagebox("Success!")
-        
-        else:
-            self.ShowMessagebox("Failed!")
+        try:
+            if all([_PerformanceCalculationConditions, _PerformanceData, _PerformanceErrorData]):
+                PerformanceCsvSave("FPS-Result.csv", f"FPS - 약 {_PerformanceCalculationConditions[benchmarkBasedTime]} ms마다 평균치 계산", _PerformanceData[FPSData])
+                PerformanceCsvSave("Frametime-Result.csv", f"Frametime", _PerformanceData[frameTimeData])
+                PerformanceCsvSave("GPUTime-Result.csv", f"GPUTime", _PerformanceData[gpuTimeData])
+                PerformanceCsvSave("Memory-Result.csv", f"Memory(MB)", _PerformanceData[memoryData])
+                PerformanceCsvSave("Frametime-Error.csv", f"Frametime error list", _PerformanceErrorData[frametimeErrorData])
+                PerformanceCsvSave("GPUTime-error.csv", f"GPUTime error list", _PerformanceErrorData[gpuTimeErrorData])
+                self.ShowMessagebox("Success!")
+                
+            else:
+                self.ShowMessagebox("Failed!")
+        except Exception as e:
+            self.ShowMessagebox(f"Error: {str(e)}")
+        finally:
+            self.deleteLater()
--- GUI/MainGUI.py
+++ GUI/MainGUI.py
@@ -249,24 +249,31 @@ class LayUpdateWorker(QWidget):
     def start(self):
         self.thread = PerformanceParsingThread(self.parent, self.FileName, self.FileData, self.benchmarkBasedTime, self.UnitConversion, self.DecimalPoint)
         self.thread.UpdateFileLabelSignal.connect(lambda text: self.StartPerformanceLable.setText(text))
+        self.thread.EmitInitializeTableSignal.connect(self.InitializeTable)
         self.thread.EmitParsedSignal.connect(self.UpdateTable)
         self.thread.EmitParsedPbarSignal.connect(self.UpdateProgressBar)
-        self.thread.ThreadFinishedSignal.connect(lambda: QTimer.singleShot(1000, lambda: None))
+        self.thread.ThreadFinishedSignal.connect(lambda: QTimer.singleShot(1000, self.UpdateTableResizeContents))
         self.thread.start()
+
+    # 테이블 초기화
+    def InitializeTable(self, label_list, col_count, row_count):
+        self.ParsedResultsTable.setColumnCount(0)    
+        self.ParsedResultsTable.setRowCount(0)
+        self.ParsedResultsTable.setColumnCount(col_count)
+        self.ParsedResultsTable.setRowCount(row_count)
+        self.ParsedResultsTable.setHorizontalHeaderLabels(label_list)
         
     # 테이블 업데이트 함수
-    def UpdateTable(self, LabelList, InitState, col_count, row_count, col, row, value):
-        if InitState:
-            self.ParsedResultsTable.setColumnCount(0)    
-            self.ParsedResultsTable.setRowCount(0)
-            self.ParsedResultsTable.setColumnCount(col_count)
-            self.ParsedResultsTable.setRowCount(row_count)
-            self.ParsedResultsTable.setHorizontalHeaderLabels(LabelList)
-
+    def UpdateTable(self, col, row, value):
         # 테이블에 데이터를 추가
         item = QTableWidgetItem(value)
         item.setTextAlignment(Qt.AlignCenter)  # 텍스트 정렬 (옵션)
         self.ParsedResultsTable.setItem(row, col, item)
+    
+    def UpdateTableResizeContents(self):
+        # 테이블 전체 크기 조정
+        self.ParsedResultsTable.resizeColumnsToContents() # 행
+        self.ParsedResultsTable.resizeRowsToContents() # 열
     
     # 프로그래스바 업데이트 함수
     def UpdateProgressBar(self, sum_pbar, sum_count):
